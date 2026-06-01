2068abc3396babcb3b6e91df25fe1ebf0aa39ddd
Morten Johansen <morten@anti.as>
2026-06-01 21:07:21 +0200

    sanity init

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ import { media, mediaAssetSource } from "sanity-plugin-media";
 export default defineConfig({
   name: "reika",
   title: "Reika Studio",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_STUDIO_PROJECT_ID || "",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_STUDIO_DATASET || "production",
+  projectId: "ncguh1wb",
+  dataset: "production",
   basePath: "/studio",
   plugins: [
     structureTool({
